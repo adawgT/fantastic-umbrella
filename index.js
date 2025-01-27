2fdc4a32265282967a63d606c64252e79b889495
adawgT <nguy1434@msu.edu>
2025-01-27 18:39:01 +0000

    added css files, and script

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 // Add JavaScript below
 import WaveSurfer from 'wavesurfer.js'
 
+<script src="https://unpkg.com/wavesurfer.js@7"></script>
+
 const wavesurfer = WaveSurfer.create({
     container: '#waveform',
     waveColor: '#4F4A85',
     progressColor: '#383351',
     url: '/audio.mp3',
   })
+
+  <script src="https://unpkg.com/wavesurfer.js@7/dist/plugins/regions.min.js"></script>
